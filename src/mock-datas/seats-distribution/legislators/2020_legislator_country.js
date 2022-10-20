289aeb005671ed76008a67142b1e63b175e66abf
caesar <caesar.lee@mirrormedia.mg>
2022-10-20 12:36:27 +0800

    chore: add mock data

---
+++ src/mock-datas/seats-distribution/legislators/2020_legislator_country.js
@@ -0,0 +1,35 @@
+// 台北市議員席次
+export const mockData = {
+  parties: {
+    regional: [
+      {
+        id: 1,
+        label: '中國國民黨',
+        short: '國民黨',
+        seats: 27,
+      },
+      {
+        id: 2,
+        label: '民主進步黨',
+        short: '民進黨',
+        seats: 19,
+      },
+    ],
+    plainIndigenous: [
+      {
+        id: 1,
+        label: '中國國民黨',
+        short: '國民黨',
+        seats: 1,
+      },
+    ],
+    mountainIndigenous: [
+      {
+        id: 1,
+        label: '中國國民黨',
+        short: '國民黨',
+        seats: 1,
+      },
+    ],
+  },
+}
